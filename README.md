2856bc9e8eafa6f729ac34da45157dd237b055b9
Julius Cueto <juliuscueto@gmail.com>
2018-06-15 23:32:05 +0900

    [mod] chaos add Runge-Kutta

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
-# python_exercises
+python_exercises
 ======
 
-## An assortment of python practices
+An assortment of python practices
 ------
 
 ### chaos
-Runge-Kutta etc for non-linear dynamics
+Codes for non-linear dynamics
+
+Key Notes
+
+* Runge-Kutta: Runge-Kutta for 2d, use of **kwargs [link](https://www.saltycrane.com/blog/2008/01/how-to-use-args-and-kwargs-in-python/)
 
 ### python_challenge
 General handling of easy input/output, original questions [src](https://github.com/zhiwehu/Python-programming-exercises). Original questions in python_exercises/questions
---
+++ chaos/runge_kutta.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+def runge_kutta_2d(fx, fy, x0, y0, t0, h, **kwargs):
+        kx1 = fx(x0, y0, t0, **kwargs)
+        ky1 = fy(x0, y0, t0, **kwargs)
+
+        kx2 = fx(x0+kx1*h/2, y0+ky1*h/2, t0+h/2, **kwargs)
+        ky2 = fy(x0+kx1*h/2, y0+ky1*h/2, t0+h/2, **kwargs)
+
+        kx3 = fx(x0+kx2*h/2, y0+ky2*h/2, t0+h/2, **kwargs)
+        ky3 = fy(x0+kx2*h/2, y0+ky2*h/2, t0+h/2, **kwargs)
+
+        kx4 = fx(x0+kx3*h, y0+ky3*h, t0+h/2, **kwargs)
+        ky4 = fy(x0+kx3*h, y0+ky3*h, t0+h/2, **kwargs)
+
+        x1 = x0 + (kx1 + 2*kx2 + 2*kx3 + kx4)*h/6
+        y1 = y0 + (ky1 + 2*ky2 + 2*ky3 + ky4)*h/6
+
+        print(kx1, ky1)
+        return x1, y1
+
+def duffing_xp(x, y, t, **kwargs):
+    return y
+
+def duffing_yp(x, y, t, **kwargs):
+    return -2*kwargs["gamma"]*y - kwargs["a"]*x - kwargs["b"]*x**3 + kwargs["F"]*np.cos(kwargs["w"]*t)
+
+if __name__ == '__main__':
+    x0 = 0.09
+    y0 = 0
+    t0 = 0
+    h = 0.01
+    vars = {"a": -1, "b": 1, "gamma": 1, "w": 1, "F":0.34875}
